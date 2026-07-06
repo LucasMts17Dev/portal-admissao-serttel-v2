5304720bc944e0e36912a65ae04dcaa68da3b832
LucasMts17Dev <lucassamuelrosa9@gmail.com>
2026-07-06 03:22:14 -0300

    corrige middleware de autenticação

--- middleware.ts
+++ middleware.ts
@@ -1,17 +1,26 @@
-import { withAuth } from 'next-auth/middleware';
-import { NextResponse } from 'next/server';
+import { NextRequest, NextResponse } from 'next/server';
+import { getToken } from 'next-auth/jwt';
 
-export default withAuth(
-  function middleware(req) {
-    return NextResponse.next();
-  },
-  {
-    callbacks: {
-      authorized: ({ token }) => !!token,
-    },
+export async function middleware(req: NextRequest) {
+  const token = await getToken({ 
+    req, 
+    secret: process.env.NEXTAUTH_SECRET 
+  });
+
+  const isAuth = !!token;
+  const isLoginPage = req.nextUrl.pathname === '/login';
+
+  if (!isAuth && !isLoginPage) {
+    return NextResponse.redirect(new URL('/login', req.url));
   }
-);
+
+  if (isAuth && isLoginPage) {
+    return NextResponse.redirect(new URL('/auditoria', req.url));
+  }
+
+  return NextResponse.next();
+}
 
 export const config = {
-  matcher: ['/auditoria/:path*'],
+  matcher: ['/auditoria/:path*', '/login'],
 };
