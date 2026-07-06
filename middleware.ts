4e4e95c60ae8516dc656cd51b7c42249f6587673
LucasMts17Dev <lucassamuelrosa9@gmail.com>
2026-07-06 03:27:10 -0300

    simplifica middleware auth

--- middleware.ts
+++ middleware.ts
@@ -1,20 +1,17 @@
 import { NextRequest, NextResponse } from 'next/server';
-import { getToken } from 'next-auth/jwt';
 
 export async function middleware(req: NextRequest) {
-  const token = await getToken({ 
-    req, 
-    secret: process.env.NEXTAUTH_SECRET 
-  });
+  const sessionToken = 
+    req.cookies.get('next-auth.session-token')?.value ||
+    req.cookies.get('__Secure-next-auth.session-token')?.value;
 
-  const isAuth = !!token;
   const isLoginPage = req.nextUrl.pathname === '/login';
 
-  if (!isAuth && !isLoginPage) {
+  if (!sessionToken && !isLoginPage) {
     return NextResponse.redirect(new URL('/login', req.url));
   }
 
-  if (isAuth && isLoginPage) {
+  if (sessionToken && isLoginPage) {
     return NextResponse.redirect(new URL('/auditoria', req.url));
   }
 
